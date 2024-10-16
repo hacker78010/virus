fb9429df4649b8e9220c34d56a1299a4a4a6b66d
hacker78010 <166924500+hacker78010@users.noreply.github.com>
2024-10-16 20:24:27 +0300

    Add files via upload

---
+++ virus1.bat
@@ -0,0 +1,14 @@
+echo off
+echo hi do you like burger
+rmdir
+echo oh dont like burger
+echo oh no
+echo no
+color a
+echo just got hacked!!!!!!!!
+echo bye your system
+echo PC time 20 seconds
+timeout 20
+echo your ipv4 
+ipconfig
+shutdown -t
